48e27fb6d89ad5f3f3b09a402465ad476cbeee13
Sorin Chis <kish.sorin@yahoo.com>
2019-03-20 14:35:07 +0200

    added get endpoint request for getting all the users

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const helmet = require('helmet');
 const postsRoutes = require('./posts-routes');
+const usersRoutes = require('./user-routes');
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 server.use('/api/posts', postsRoutes);
+server.use(usersRoutes);
 
 server.get('/', (req, res) => {
 	res.send('<h1>Hello from the Server</h1>');
