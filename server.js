c0debfe0ee14392c6ae79b27b5b61d3184539174
Sorin Chis <kish.sorin@yahoo.com>
2019-03-20 14:22:42 +0200

    added get endpoint request for getting all the posts from db

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const helmet = require('helmet');
+const postsRoutes = require('./posts-routes');
+
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
+server.use('/api/posts', postsRoutes);
 
 server.get('/', (req, res) => {
 	res.send('<h1>Hello from the Server</h1>');
