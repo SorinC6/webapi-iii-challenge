b1133074892fa38168058bc9d54874f7edc5877f
Sorin Chis <kish.sorin@yahoo.com>
2019-03-20 15:15:04 +0200

    added endpoint request for deleting a user

--- user-routes.js
+++ user-routes.js
@@ -50,7 +50,7 @@ userRoutes.post('/', (req, res) => {
 		userDb
 			.insert(user)
 			.then((user) => {
-				res.json(user);
+				res.status(201).json(user);
 			})
 			.catch((err) => {
 				res.status(500).json({ error: 'There was an error while saving the post to the database' });
@@ -59,5 +59,21 @@ userRoutes.post('/', (req, res) => {
 		res.status(400).json({ errorMessage: 'Please provide name for the user.' });
 	}
 });
+
+userRoutes.delete('/:id', async (req, res) => {
+	const { id } = req.params;
+	try {
+		const result = await userDb.remove(id);
+		if (result.length) {
+			res.status(200).json(result);
+		} else {
+			res.status(404).json({ message: 'The user with the specified ID does not exist.' });
+		}
+	} catch (error) {
+		res.status(500).json({ error: 'The user could not be removed' });
+	}
+});
+
+
 
 module.exports = userRoutes;
