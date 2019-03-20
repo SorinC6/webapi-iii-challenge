b3bac02206f334bd5c674e3ef2d66ff1dfee1095
Sorin Chis <kish.sorin@yahoo.com>
2019-03-20 14:04:47 +0200

    added helmet protection to server

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
-
+const helmet = require('helmet');
 const server = express();
 
 server.use(express.json());
+server.use(helmet());
 
 server.get('/', (req, res) => {
 	res.send('<h1>Hello from the Server</h1>');
