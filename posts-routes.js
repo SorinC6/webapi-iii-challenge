49549292ae13fce758e2e5b9bb5799d3eaea1d9c
Sorin Chis <kish.sorin@yahoo.com>
2019-03-20 15:49:08 +0200

    added endpoint  for getting a post by id

--- posts-routes.js
+++ posts-routes.js
@@ -10,4 +10,19 @@ postsRoutes.get('/', (req, res) => {
 		.catch((err) => res.status(500).json({ error: 'The posts information could not be retrieved.' }));
 });
 
+postsRoutes.get('/:id', async (req, res) => {
+	const { id } = req.params;
+
+	try {
+		const post = await postDb.getById(id);
+		if (post) {
+			res.status(200).json(post);
+		} else {
+			res.status(404).json({ message: 'The post with the specified ID does not exist.' });
+		}
+	} catch (error) {
+		res.status(500).json({ error: 'The post information could not be retrieved.' });
+	}
+});
+
 module.exports = postsRoutes;
