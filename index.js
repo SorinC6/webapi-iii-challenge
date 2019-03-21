95a4784e7c27246318bae56531c33b229d68566b
Sorin Chis <kish.sorin@yahoo.com>
2019-03-21 13:50:08 +0200

    final commit, ready to deploy on heroku

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// code away!
 require('dotenv').config();
+
 const server = require('./server');
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 4000;
 
-server.listen(4000, () => {
+server.listen(PORT, () => {
 	console.log(`\n*** Server running on http://localhost:${PORT} ***\n`);
 });
